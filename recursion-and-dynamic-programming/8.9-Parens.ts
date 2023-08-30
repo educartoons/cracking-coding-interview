6d9dd95f8f3f02f37c5a982b8d0474e376c15d58
Eduar <educartoons@gmail.com>
2023-08-29 20:15:47 -0500

    added new way to Paren problem

--- recursion-and-dynamic-programming/8.9-Parens.ts
+++ recursion-and-dynamic-programming/8.9-Parens.ts
@@ -34,14 +34,16 @@ function addParen(list: Array<string>, leftRem: number, rightRem: number, str: A
   if(leftRem<0 || rightRem<leftRem) return;
 
   if(leftRem === 0 && rightRem === 0){
-    
     list.push(str.join(''));
   }else{
-    
     str[index] = '(';
+    console.log(leftRem, index, str)
+    
     addParen(list, leftRem-1, rightRem, str, index+1);
 
     str[index] = ')';
+    console.log(leftRem, index, str)
+    
     addParen(list, leftRem, rightRem-1, str, index+1);
   }
 
@@ -56,4 +58,4 @@ function generateParenthesis(num: number): Array<string>{
 }
 
 
-console.log(generateParenthesis(3))
+console.log(generateParenthesis(2))
